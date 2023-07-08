80958890377f1873b826a7606a15b1b50973bb33
Cristian da Gonzatlán <cristhiangonzalez06@icloud.com>
2023-07-08 11:00:43 -0500

    Add Firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { AuthModule } from './pages/auth/auth.module';
 import { FlowBoardModule } from './pages/flowboard/flowboard.module';
 
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, FlowBoardModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    FlowBoardModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
